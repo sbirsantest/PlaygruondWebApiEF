83107aade14319ec99a8f2a73a1dced6efb05488
sbarsan <sorinbarsan@gmail.com>
2022-01-26 17:40:51 +0200

    prepare for running migrations

--- WebApi.DataAccess/Data/Contexts/AdministationDbContext.cs
+++ WebApi.DataAccess/Data/Contexts/AdministationDbContext.cs
@@ -4,17 +4,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebApi.Domain.Config;
 using WebApi.Domain.Models;
 
 namespace WebApi.DataAccess.Data.Contexts
 {
     public class AdministationDbContext : DbContext
     {
+        private readonly GlobalConfig _config;
+
         public DbSet<Organisation> Organisations { get; set; }
         public DbSet<User> Users { get; set; }
 
-        public AdministationDbContext(DbContextOptions<AdministationDbContext> options) : base(options)
+        // constructor used at design time
+        internal AdministationDbContext(DbContextOptions<AdministationDbContext> options) : base(options)
         {
         }
+
+        public AdministationDbContext(DbContextOptions<AdministationDbContext> options, GlobalConfig config) : base(options)
+        {
+            _config = config;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var connectString = GetConnectionString();
+                optionsBuilder.UseSqlServer(connectString);
+
+                //// for azure https://docs.microsoft.com/en-us/dotnet/architecture/microservices/implement-resilient-applications/implement-resilient-entity-framework-core-sql-connections
+                //optionsBuilder.UseSqlServer(connectString, sqlOptions =>
+                //{
+                //    sqlOptions.EnableRetryOnFailure(
+                //    maxRetryCount: 5,
+                //    maxRetryDelay: TimeSpan.FromSeconds(30),
+                //    errorNumbersToAdd: null);
+                //});
+                //optionsBuilder.UseLoggerFactory(_loggerFactory);
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected virtual string GetConnectionString()
+        {
+            return string.IsNullOrEmpty(_config.AdminDbPassword) ?
+                _config.AdminDbConnectionString : $"Password=\"{_config.AdminDbPassword}\";{_config.AdminDbConnectionString}";
+        }
+
     }
 }
---
+++ WebApi.DataAccess/Data/Contexts/DesignTime/DesignTimeContextFactory.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.DataAccess.Data.Contexts.DesignTime
+{
+    public class DesignTimeContextFactory : IDesignTimeDbContextFactory<AdministationDbContext>
+    {
+        const string AdminDbConnectionString = "WebApiAdminDbConnectionString";
+
+        private static DbContextOptionsBuilder<T> SetupOptions<T>(string envName) where T : DbContext
+        {
+            // set first the envvariable for the connection string to be used when run migrations!!!
+            // run the following command WITH THE RIGHT CONN STRING
+            // $env:WebApiAdminDbConnectionString = "Server=PC310094\\SQLEXPRESS;Database=webapi_administation;User Id=sorinbtest;MultipleActiveResultSets=false;Encrypt=False;Application Name=WebApi;Password=Password123!@#"
+            var adminDbConnectionString = Environment.GetEnvironmentVariable(envName);
+            if (string.IsNullOrEmpty(adminDbConnectionString))
+                throw new Exception($"Please set connection string via environment variable '{envName}. \nAlso.. try not to run live migrations against live db, run dotnet ef ... script to examine them first!");
+
+            var optionsBuilder = new DbContextOptionsBuilder<T>().UseSqlServer(adminDbConnectionString);
+            optionsBuilder.LogTo(x => Console.WriteLine($"debug: {x}"));
+            optionsBuilder.EnableSensitiveDataLogging();
+            return optionsBuilder;
+
+        }
+
+        AdministationDbContext IDesignTimeDbContextFactory<AdministationDbContext>.CreateDbContext(string[] args)
+        {
+            var optionsBuilder = SetupOptions<AdministationDbContext>(AdminDbConnectionString);
+            return new AdministationDbContext(optionsBuilder.Options);
+        }
+    }
+}
