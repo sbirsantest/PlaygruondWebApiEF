c9f5190154f4f06c7a116fd7941f6b60a4d00824
sbarsan <sorinbarsan@gmail.com>
2022-01-26 16:56:24 +0200

    add new folder Data to the DtaAccess project and move the Contexts folder there

--- WebApi.DataAccess/Data/Contexts/AdministationDbContext.cs
+++ WebApi.DataAccess/Data/Contexts/AdministationDbContext.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using WebApi.Domain.Models;
 
-namespace WebApi.DataAccess.Contexts
+namespace WebApi.DataAccess.Data.Contexts
 {
     public class AdministationDbContext : DbContext
     {
