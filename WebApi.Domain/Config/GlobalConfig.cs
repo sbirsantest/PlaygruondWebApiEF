026f517c73db27816a781068e0754a7a821e62b7
sbarsan <sorinbarsan@gmail.com>
2022-01-26 16:38:08 +0200

    add the global configuration file; this is registered to DI container to be used when needed; ths password was set in the UserCredentials

---
+++ WebApi.Domain/Config/GlobalConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.Domain.Config
+{
+    public class GlobalConfig
+    {
+        public const string SectionName = "WebApi";
+
+        public string AdminDbConnectionString { get; set; } = "unconfigured";
+
+        public string AdminDbPassword { get; set; } = "unconfigured";
+    }
+}
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApi.Domain.Config;
 
 namespace WebApi
 {
@@ -26,6 +27,9 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var settings = new GlobalConfig();
+            Configuration.Bind(GlobalConfig.SectionName, settings);
+            services.AddSingleton(settings);
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
