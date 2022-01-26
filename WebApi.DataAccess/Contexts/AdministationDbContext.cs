20f55a0fde9d66257e5f610fd3de26e883d876e1
sbarsan <sorinbarsan@gmail.com>
2022-01-26 16:20:35 +0200

    install Microsoft.EntityFrameworkCore, add class for the context with the needed datasets and constructor

---
+++ WebApi.DataAccess/Contexts/AdministationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Domain.Models;
+
+namespace WebApi.DataAccess.Contexts
+{
+    public class AdministationDbContext : DbContext
+    {
+        public DbSet<Organisation> Organisations { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public AdministationDbContext(DbContextOptions<AdministationDbContext> options) : base(options)
+        {
+        }
+    }
+}
