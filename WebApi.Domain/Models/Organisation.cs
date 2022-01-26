bd0efb351221e9a3fa46dfe3adae63c93ec2a9ab
sbarsan <sorinbarsan@gmail.com>
2022-01-26 16:09:27 +0200

    add user and organisation models

---
+++ WebApi.Domain/Models/Organisation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.Domain.Models
+{
+    public class Organisation
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Organisation> Organisations { get; set; }
+    }
+}
