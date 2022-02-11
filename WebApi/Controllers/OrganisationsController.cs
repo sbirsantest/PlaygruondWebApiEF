e5b0e1992f425ccc5f62b7f3b3ee9712474a3a69
sbarsan <sorinbarsan@gmail.com>
2022-02-11 16:07:40 +0200

    add organisation controller (just Get all). fix connection stringm delete https from launch settings

---
+++ WebApi/Controllers/OrganisationsController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.DataAccess.Data.Contexts;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrganisationsController : ControllerBase
+    {
+        private readonly AdministrationDbContext _administrationDbContext;
+
+        public OrganisationsController(AdministrationDbContext administrationDbContext)
+        {
+            _administrationDbContext = administrationDbContext;
+        }
+
+        // GET: api/<OrganisationsController>
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _administrationDbContext.Organisations.ToListAsync());
+        }
+
+        //// GET api/<OrganisationsController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<OrganisationsController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<OrganisationsController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<OrganisationsController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
